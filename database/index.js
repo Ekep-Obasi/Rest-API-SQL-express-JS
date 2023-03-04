17970e3b7b0c1b5af84ab83782d73388768f7432
Ekep Obasi <arreyettaekep@gmail.com>
2023-03-04 23:48:33 +0100

    Implemented authorisation

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("drinks_db", "root", undefined, {
-  host: "localhost",
+const sequelize = new Sequelize("drinksdb", "ekepobasi", "drinksdb123", {
+  host: "db4free.net",
   dialect: "mysql",
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,32 @@
 const express = require("express");
+const bycrypt = require("bcrypt");
+const uuid = require("uuid");
 const Drink = require("../database/Model/drink");
 const User = require("../database/Model/users");
+const SALT_ROUNDS = require("../constants");
 const router = express.Router();
-
 router.get("/", async function (_, res) {
   const users = await User.findAll({ include: Drink });
   res.send(users);
 });
 
-router.post("/", async function (req, res) {
+router.post("/", function (req, res) {
   const { firstName, lastName, emailAddress, phone, password } = req.body;
-  const user = await User.create({
-    firstName,
-    lastName,
-    emailAddress,
-    phone,
-    password,
-    apiKey: Date.now(),
+  bycrypt.hash(password, SALT_ROUNDS, async (err, hash) => {
+    console.log(hash);
+    if (err) res.status(500).send(err);
+    else {
+      const user = await User.create({
+        firstName,
+        lastName,
+        emailAddress,
+        phone,
+        password: hash,
+        apiKey: uuid.v4(),
+      });
+      res.send(user);
+    }
   });
-  res.send(user);
 });
 
 router.get("/:id", async function (req, res) {
